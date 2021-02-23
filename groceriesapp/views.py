578d274f243d07d5fdc30fc011d731008448d36a
Ankit jha <65860426+ankit0003jha@users.noreply.github.com>
2021-02-23 11:45:25 +0530

    Update views.py
    1

--- groceriesapp/views.py
+++ groceriesapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 
-### This view section is for the template purpose for api view check views.py  
+### This view section is for the template purpose for api view check viewset.py  
