be5af1b4f57a53735c0d8cec9cfb3c44130ea07d
Ankit jha <65860426+ankit0003jha@users.noreply.github.com>
2021-02-23 14:22:57 +0530

    Update views.py
    1

--- groceriesapp/views.py
+++ groceriesapp/views.py
@@ -25,27 +25,28 @@ def index(request):
 
 
 
-@api_view(['GET', 'POST', 'DELETE'])
-def Grocery_list(request):
-    if request.method == 'GET':
-        Grocerys = GroceryItem.objects.all()
-        
-        title = request.GET.get('title', None)
-        if title is not None:
-            Grocerys = Grocerys.filter(title__icontains=title)
-        
-        GroceryItem_serializer = GroceryItemSerializer(Grocerys, many=True)
-        return JsonResponse(GroceryItem_serializer.data, safe=False)
-        # 'safe=False' for objects serialization
- 
-    elif request.method == 'POST':
-        groceryItem_data = JSONParser().parse(request)
-        GroceryItem_serializer = GroceryItemSerializer(data=GroceryItem_data)
-        if GroceryItem_serializer.is_valid():
-            GroceryItem_serializer.save()
-            return JsonResponse(GroceryItem_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(GroceryItem_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        grocery = GroceryItem.objects.all().delete()
-        return JsonResponse({'message': '{} Groceryitem were deleted successfully!'.format(grocery[0])}, status=status.HTTP_204_NO_CONTENT)
+#@api_view(['GET', 'POST', 'DELETE'])
+#def Grocery_list(request):
+#    if request.method == 'GET':
+#        Grocerys = GroceryItem.objects.all()
+#        
+#        title = request.GET.get('title', None)
+#        if title is not None:
+#            Grocerys = Grocerys.filter(title__icontains=title)
+#        
+#        GroceryItem_serializer = GroceryItemSerializer(Grocerys, many=True)
+#        return JsonResponse(GroceryItem_serializer.data, safe=False)
+#        # 'safe=False' for objects serialization
+# 
+#    elif request.method == 'POST':
+#        groceryItem_data = JSONParser().parse(request)
+#        GroceryItem_serializer = GroceryItemSerializer(data=GroceryItem_data)
+#        if GroceryItem_serializer.is_valid():
+#            GroceryItem_serializer.save()
+#            return JsonResponse(GroceryItem_serializer.data, status=status.HTTP_201_CREATED) 
+#        return JsonResponse(GroceryItem_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#    
+#    elif request.method == 'DELETE':
+#        grocery = GroceryItem.objects.all().delete()
+#        return JsonResponse({'message': '{} Groceryitem were deleted successfully!'.format(grocery[0])}, status=status.HTTP_204_NO_CONTENT)
+#
