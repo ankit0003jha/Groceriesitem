89752acaa5ee3ff1e02fb12f320a3665dd2788e6
Ankit jha <65860426+ankit0003jha@users.noreply.github.com>
2021-02-23 12:33:59 +0530

    Update views.py
    1

--- groceriesapp/views.py
+++ groceriesapp/views.py
@@ -2,4 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 
-### This view section is for the template purpose for api view check viewset.py  
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from .serializers import GroceryItemSerializer
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+import json
+
+from groceriesapp.models import GroceryItem
+
+def index(request):
+    response = json.dumps([{}])
+    return HttpResponse(response, content_type='text/json')
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def Grocery_list(request):
+    if request.method == 'GET':
+        Grocerys = GroceryItem.objects.all()
+        
+        title = request.GET.get('title', None)
+        if title is not None:
+            Grocerys = Grocerys.filter(title__icontains=title)
+        
+        GroceryItem_serializer = GroceryItemSerializer(Grocerys, many=True)
+        return JsonResponse(GroceryItem_serializer.data, safe=False)
+        # 'safe=False' for objects serialization
+ 
+    elif request.method == 'POST':
+        groceryItem_data = JSONParser().parse(request)
+        GroceryItem_serializer = GroceryItemSerializer(data=GroceryItem_data)
+        if GroceryItem_serializer.is_valid():
+            GroceryItem_serializer.save()
+            return JsonResponse(GroceryItem_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(GroceryItem_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        count = GroceryItem.objects.all().delete()
+        return JsonResponse({'message': '{} Grocery were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
